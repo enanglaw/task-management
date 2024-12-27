6b4fa207c493063c10cea789c89543bc65574c5e
Lawrence Enang <lenang@ecobank.com>
2024-12-27 12:03:18 +0100

    fix prettier issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import {TypeOrmModule} from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { TaskRepository } from './tasks/task.repository';
 @Module({
   imports: [
-    
     TypeOrmModule.forRoot({
-        type:'postgres',
-        host:'localhost',
-        port:5432,
-        password:'@A1w2a3s4e5',
-        username:'postgres',
-        database:'task-management',
-        autoLoadEntities:true,
-        synchronize:true
-
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '@A1w2a3s4e5',
+      username: 'postgres',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     TasksModule,
-    AuthModule],
+    AuthModule,
+  ],
 })
 export class AppModule {}
